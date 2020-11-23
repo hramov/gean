0dfde8861301b58184b5bb56cf66e051c8d403cb
hramov <trykhramov@gmail.com>
2020-11-23 10:35:59 +0300

    Auto commit 23.11.2020

--- analyze/statistics.js
+++ analyze/statistics.js
@@ -1,3 +1,14 @@
-export default async function statistics() {
+import { getSongs } from './../FBController'
+import _ from 'underscore'
 
+export default async function statistics(id_artist, data) {
+
+    const words = await getSongs(id_artist) // ['','','']
+    words = words.append(data).join(' ').split(' ')
+    words = _.unique(words)
+
+    return {
+        words: words, // ['','',''] Array of all unique words for the particular artist
+        count: words.length // Number of unique words for the particular artist
+    }
 }
--- index.js
+++ index.js
@@ -4,21 +4,21 @@ dotenv.config()
 import { searchSongsAndContent } from './search'
 import { checkWords } from './analyze'
 import { sendData } from './FBController'
-import { checkLogFile, log } from './utils'
 import statistics from './analyze/statistics'
 
+import { checkLogFile, log } from './utils'
 import config from './config.json'
+
+const artists = config.artists
 
 async function index() {
 
-    for (let i = 0; i < config.artists.length; i++) {
+    for (let i = 0; i < artists.length; i++) {
 
         let result
         result = await searchSongsAndContent(artists[i])
         result = result.map(song => checkWords(song))
-
-        await sendData(result)
-        await statistics(result)
+        await sendData(statistics(artists[i].id, result.join(' ')))
     }
 
 }
